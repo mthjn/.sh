deddec831aa3a2577222016e5d2ff3a592e962c8
mthjn <jana.mathauserova@ubk.cz>
2015-07-31 16:02:38 +0200

    add typo3

---
+++ ubuntu1404_nginx_typo3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# configure block afterwards
+# dep mysql, php, nginx
+# needs empty db
+
+# dir struct
+cd /var/www/
+sudo su
+mkdir -p typo.test/www
+
+apt-get install php-apc
+# for php-fpm as FastCGI deamon
+service php5-fpm restart
+
+cd /tmp
+wget http://downloads.sourceforge.net/typo3/typo3_src-7.3.1.tar.gz
+tar xvfz typo3_src-7.3.1.tar.gz
+cd /var/www/typo.test/www/
+sudo rsync -avP /home/$USER/typo3_src-7.3.1/* ./

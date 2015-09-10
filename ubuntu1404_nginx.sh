b1a09783cc663293efdedb035f77a1f850a26003
mthjn <mthjn2@gmail.com>
2015-09-10 16:41:32 +0200

    add force yes

--- ubuntu1404_nginx.sh
+++ ubuntu1404_nginx.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-# nginx on ubuntu 1404
+
+# nginx on ubuntu 14.04
 # sudo su 
 # chmod 777
 
 apt-get update
-apt-get install nginx
+apt-get install nginx -y
 
 sudo update-rc.d nginx defaults
+
+
